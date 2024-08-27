ff057bf22de7e92914f5198d8acd813de39689fa
VP-Bhatt <34vedprakash10@gmail.com>
2024-08-27 18:06:17 +0000

    add http server

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
+
+@RestController
+class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello world!";
+    }
+
+}
