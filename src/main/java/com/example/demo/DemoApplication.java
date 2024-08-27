b8e644d64a3d4f0de1c7cc8913ffbd8bd669e001
VP-Bhatt <34vedprakash10@gmail.com>
2024-08-27 18:31:36 +0000

    add latest changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,19 @@
 package com.example.demo;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.cloud.vertexai.VertexAI;
+import com.google.cloud.vertexai.api.GenerateContentResponse;
+import com.google.cloud.vertexai.generativeai.GenerativeModel;
+import com.google.cloud.vertexai.generativeai.ResponseHandler;
+
+import com.google.cloud.ServiceOptions;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -17,9 +27,17 @@ public class DemoApplication {
 @RestController
 class HelloController {
 
+    String projectId = ServiceOptions.getDefaultProjectId();
+
     @GetMapping("/")
-    public String hello() {
-        return "Hello world!";
+    public String getFacts(@RequestParam(defaultValue = "dog") String animal) throws IOException {
+        String prompt = "Give me 10 fun facts about " + animal + ". Return this as html without backticks.";
+
+        VertexAI vertexAI = new VertexAI(projectId, null);
+        GenerativeModel model = new GenerativeModel("gemini-1.5-flash", vertexAI);
+
+        GenerateContentResponse response = model.generateContent(prompt);
+        return ResponseHandler.getText(response);
     }
 
 }
